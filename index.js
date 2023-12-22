5bec780db24d21a99e6cf4954ee311ce7ddab381
ang <angelo03giuliano@gmail.com>
2023-12-22 10:46:49 +0100

    up search

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const searchField = document.getElementById("searchField");
 const searchSection = document.getElementById("searchSection");
+let valoreRicerca;
 
-const fetchSongs = () => {
-  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=charlie")
+searchField.addEventListener("change", function () {
+  valoreRicerca = searchField.value;
+  fetchSongs(valoreRicerca);
+});
+
+const fetchSongs = (ricerca) => {
+  fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${ricerca}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.data[0]);
-      for(let i = 0; i < data.data.length; i++) {
+      searchSection.innerHTML = "";
+      console.log(data.data);
+      for (let i = 0; i < data.data.length; i++) {
         searchSection.innerHTML += `
             <div class="card" style="width: 10rem;">
                 <img src=${data.data[i].album.cover_medium} class="card-img-top w-100 p-1" alt="...">
@@ -14,9 +22,8 @@ const fetchSongs = () => {
                     <p class="card-text">${data.data[i].artist.name}</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
-            </div>`
+            </div>
+            `
       }
     });
 };
-
-fetchSongs();
